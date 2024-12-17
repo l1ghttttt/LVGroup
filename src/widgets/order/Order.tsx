90f72b18d02fbb9c9fbe8fc8336fc1ebbdb2e8c3
nik0504030201 <nikita0504030201@gmail.com>
2024-12-17 23:53:58 +0500

    contacts

--- src/widgets/order/Order.tsx
+++ src/widgets/order/Order.tsx
@@ -42,19 +42,13 @@ const Order = () => {
                                 </Link>
                             </li>*/}
                             <li className={`mr-[40px] mb-[50px] max-2xl:mr-[30px] max-2xl:mb-[30px] max-sm:mr-[20px] max-sm:mb-[20px] max-sm:w-auto`}>
-                                <Link href="https://t.me/tvoi_dvigatel"
+                                <Link target={`_blank`} href="https://t.me/tvoi_dvigatel"
                                       className={`touch-manipulation text-orderDescrSize leading-orderDescrLeading flex`}>
-                                    <svg
-                                        className="w-6 h-6 text-gray-800 dark:text-white mr-orderIconMargin"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="33"
-                                        height="27"
-                                        fill="currentColor"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            d="m3 2 1.578 17.824L12 22l7.467-2.175L21 2H3Zm14.049 6.048H9.075l.172 2.016h7.697l-.626 6.565-4.246 1.381-4.281-1.455-.288-2.932h2.024l.16 1.411 2.4.815 2.346-.763.297-3.005H7.416l-.562-6.05h10.412l-.217 2.017Z"/>
+                                    <svg className="w-6 h-6 text-gray-800 dark:text-white mr-orderIconMargin"  aria-hidden="true" fill="currentColor"
+                                         xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="33" height="27"
+                                         viewBox="0 0 50 50">
+                                        <path fill="currentColor"
+                                              d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
                                     </svg>
                                     telegram
                                 </Link>
