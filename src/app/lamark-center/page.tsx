583e4c633d4fa9fe2c08a87a0243eb6b12c5c3a4
nik0504030201 <nikita0504030201@gmail.com>
2025-02-05 22:24:14 +0500

    new image mobile fixes

--- src/app/lamark-center/page.tsx
+++ src/app/lamark-center/page.tsx
@@ -146,11 +146,10 @@ const Ldpr = () => {
                             </div>
                         </div>
                         <div
-                            className={`rounded-[20px] border-[8px] border-black w-[90%] mt-[45px] max-lg:gap-[20px] relative`}>
-                            <BeforeAfterSlider firstImage={firstImage} secondImage={secondImage} className={``}
-                                               delimiterColor={`black`}/>
-                            <p className={`absolute bottom-[-3px] left-0 text-[18px] font-railway text-mainColor bg-black p-[8px] rounded-tr-[7px]`}>Было</p>
-                            <p className={`absolute bottom-[-3px] right-0 text-[18px] font-railway text-mainColor bg-black p-[8px] rounded-tl-[7px]`}>Стало</p>
+                            className={`rounded-[20px] border-[8px] border-black w-[90%] max-md:w-[95vw] mt-[45px] max-lg:gap-[20px] relative`}>
+                            <BeforeAfterSlider firstImage={firstImage} secondImage={secondImage} delimiterColor={`black`}/>
+                            <p className={`absolute bottom-[-3px] left-0 text-[16px] max-md:text-[12px] font-railway text-mainColor bg-black p-[8px] max-md:p-[6px] rounded-tr-[7px]`}>Было</p>
+                            <p className={`absolute bottom-[-3px] right-0 text-[16px] max-md:text-[12px] font-railway text-mainColor bg-black p-[8px] max-md:p-[6px] rounded-tl-[7px]`}>Стало</p>
                         </div>
 
                         <div
@@ -336,20 +335,20 @@ const Ldpr = () => {
                         </div>
 
                         <div
-                            className={`w-full flex flex-col gap-[25px] mt-[45px] max-lg:gap-[20px] max-lg:mt-[20px] items-start`}>
+                            className={`w-full flex flex-col gap-[25px] mt-[45px] max-lg:gap-[20px]  max-md:justify-end max-lg:mt-[20px] items-start`}>
                             <h2 className={`text-customerClientSize text-mainColor`}>Отзыв клиента</h2>
                             <div className={`h-[1px] bg-foreground w-full`}></div>
 
 
-                            <div className={`w-full flex  max-lg:gap-[20px] max-lg:mt-[20px] gap-[40px]`}>
+                            <div className={`w-full flex max-lg:gap-[20px] max-lg:mt-[10px] max-md:gap-0 max-md:flex-col`}>
 
 
-                                <div className={`flex w-2/6 flex-col justify-between pt-3`}>
-                                    <Image className={`rotate-180`} src={'/arrows.png'} alt={`arrows`} width={50}
+                                <div className={`flex w-2/6 max-md:w-full flex-col justify-between pt-3 max-md:order-2 max-md:pt-2`}>
+                                    <Image className={`rotate-180 max-md:hidden`} src={'/arrows.png'} alt={`arrows`} width={50}
                                            height={50}/>
-                                    <div className={`w-full flex justify-start`}>
+                                    <div className={`w-full flex justify-start max-md:justify-end`}>
                                         <div className={`flex gap-[10px]`}>
-                                            <Image className={`rounded-[50%]`} src={`/lamark-director.png`}
+                                            <Image className={`rounded-[50%] max-md:w-[60px] max-md:h-[60px]`} src={`/lamark-director.png`}
                                                    alt={`фото клиента`}
                                                    width={75} height={75}/>
                                             <div className={`flex flex-col justify-center`}>
@@ -361,8 +360,8 @@ const Ldpr = () => {
                                 </div>
 
 
-                                <div className={`flex w-4/6`}>
-                                    <p className={`w-full text-[22px] max-md:w-full  mb-[25px]`}><span
+                                <div className={`flex w-4/6 max-md:w-full`}>
+                                    <p className={`w-full text-[22px] max-lg:text-[18px] max-sm:text-[16px] max-md:w-full mb-[25px] max-md:mb-0`}><span
                                         className={`text-[30px] font-semibold leading-[20px]`}>&quot; </span>Обратились
                                         за
                                         созданием сайта в компанию &quot;Двигатель&quot;. Ребята отнеслись креативно, с
