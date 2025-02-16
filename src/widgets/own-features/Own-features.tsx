abf1d202cfa0df4003b855ad30b80b324a43a602
nik0504030201 <nikita0504030201@gmail.com>
2025-02-17 00:26:16 +0500

    icon changes

--- src/widgets/own-features/Own-features.tsx
+++ src/widgets/own-features/Own-features.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { IoStatsChartSharp } from "react-icons/io5";
-import { LuMonitorSmartphone } from "react-icons/lu";
-import { IoShieldCheckmark } from "react-icons/io5";
-import { PiSealPercentFill } from "react-icons/pi";
-import { FaRegCalendarCheck } from "react-icons/fa";
-import { BiLogoFigma } from "react-icons/bi";
+import {CiBookmarkCheck, CiBoxList, CiCalendar, CiDiscount1, CiGrid32, CiMonitor} from "react-icons/ci";
 
 const OwnFeatures = () => {
     return (
@@ -14,71 +9,71 @@ const OwnFeatures = () => {
                     вы получаете, работая с нами</h2>
             </div>
             <div className={`w-full flex justify-center`}>
-                <ul className={`flex flex-wrap w-[90%] justify-between gap-y-[40px]`}>
-                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center h-[200px]`}>
-                        <div className={`w-[100px] h-[100px]`}>
-                            <LuMonitorSmartphone size={100}/>
+                <ul className={`flex flex-wrap w-[90%] max-xl:w-[75%] max-md:w-full justify-between gap-y-[40px]`}>
+                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center xl:h-[200px]`}>
+                        <div className={`w-[100px] h-[100px] max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px] max-md:flex max-md:items-center`}>
+                            <CiMonitor size={100} className={`max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px]`}/>
                         </div>
-                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px]`}>
-                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway`}>Современный и адаптивный сайт</h3>
-                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr  max-md:mt-[10px]`}>Используем на сайтах эффективные сервисы: квизы, формы обратного звонка, сервисы для глубокой аналитики рекламы и калькуляторы стоимости. Пишем чистый и валидный код.</p>
+                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px] max-xl:justify-around`}>
+                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway max-2xl:text-[26px] max-md:text-[24px] max-sm:text-[22px] max-md:leading-[32px]`}>Современный и адаптивный сайт</h3>
+                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr max-sm:mt-0  max-md:mt-[10px] max-2xl:text-[16px] max-md:text-[15px] max-sm:text-[14px] max-sm:leading-[22px]`}>Используем на сайтах эффективные сервисы: квизы, формы обратной связи, сервисы для глубокой аналитики, калькуляторы стоимости. Пишем чистый и валидный код.</p>
                         </div>
                     </li>
-                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center h-[200px]`}>
-                        <div className={`w-[100px] h-[100px]`}>
-                            <BiLogoFigma size={100}/>
+                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center xl:h-[200px]`}>
+                        <div className={`w-[100px] h-[100px] max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px] max-md:flex max-md:items-center`}>
+                            <CiGrid32 size={100} className={`max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px]`}/>
                         </div>
-                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px]`}>
-                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway`}>Экспертность
+                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px] max-xl:justify-around`}>
+                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway max-2xl:text-[26px] max-md:text-[24px] max-sm:text-[22px] max-md:leading-[32px]`}>Экспертность
                                 в дизайне и создании контента</h3>
-                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr  max-md:mt-[10px]`}>Проводим
+                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr max-sm:mt-0  max-md:mt-[10px] max-2xl:text-[16px] max-md:text-[15px] max-sm:text-[14px] max-sm:leading-[22px]`}>Проводим
                                 съёмки, создаем иллюстрации, напишем тексты, исходя из современных и актуальных
                                 трендов.</p>
                         </div>
                     </li>
-                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center h-[200px]`}>
-                        <div className={`w-[100px] h-[100px]`}>
-                            <IoShieldCheckmark size={100}/>
+                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center xl:h-[200px]`}>
+                        <div className={`w-[100px] h-[100px] max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px] max-md:flex max-md:items-center`}>
+                            <CiBookmarkCheck size={100} className={`max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px]`}/>
                         </div>
-                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px]`}>
-                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway`}>Помощь
+                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px] max-xl:justify-around`}>
+                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway max-2xl:text-[26px] max-md:text-[24px] max-sm:text-[22px] max-md:leading-[32px]`}>Помощь
                                 и обучение работе с сайтом</h3>
-                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr  max-md:mt-[10px]`}>Поможем с CMS, научим обновлять контент сайта,
+                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr max-sm:mt-0 max-md:mt-[10px] max-2xl:text-[16px] max-md:text-[15px] max-sm:text-[14px] max-sm:leading-[22px]`}>Поможем с CMS, научим обновлять контент сайта,
                                 поможем продлить сертификат SSL, сменить хостинг или домен сайта и многое
                                 другое.</p>
                         </div>
                     </li>
-                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center h-[200px]`}>
-                        <div className={`w-[100px] h-[100px]`}>
-                            <IoStatsChartSharp size={100}/>
+                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center xl:h-[200px]`}>
+                        <div className={`w-[100px] h-[100px] max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px] max-md:flex max-md:items-center`}>
+                            <CiBoxList size={100} className={`max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px]`}/>
                         </div>
-                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px]`}>
-                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway`}>Доступ
+                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px] max-xl:justify-around`}>
+                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway max-2xl:text-[26px] max-md:text-[24px] max-sm:text-[22px] max-md:leading-[32px]`}>Доступ
                                 к статистике и отчетам сайта</h3>
-                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr  max-md:mt-[10px]`}>Предоставляем
+                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr max-sm:mt-0  max-md:mt-[10px] max-2xl:text-[16px] max-md:text-[15px] max-sm:text-[14px] max-sm:leading-[22px]`}>Предоставляем
                                 постоянный доступ к онлайн статистике, регулярно отчитываемся о выполненных работах по
                                 сайту.</p>
                         </div>
                     </li>
-                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center h-[200px]`}>
-                        <div className={`w-[100px] h-[100px]`}>
-                            <PiSealPercentFill size={100}/>
+                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center xl:h-[200px]`}>
+                        <div className={`w-[100px] h-[100px] max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px] max-md:flex max-md:items-center`}>
+                            <CiDiscount1 size={100} className={`max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px]`}/>
                         </div>
-                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px]`}>
-                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway`}>Скидки</h3>
-                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr  max-md:mt-[10px]`}>Предоставляем
+                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px] max-xl:justify-around`}>
+                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway max-2xl:text-[26px] max-md:text-[24px] max-sm:text-[22px] max-md:leading-[32px]`}>Скидки</h3>
+                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr max-sm:mt-0  max-md:mt-[10px] max-2xl:text-[16px] max-md:text-[15px] max-sm:text-[14px] max-sm:leading-[22px]`}>Предоставляем
                                 скидки на сопутствующие услуги для максимально эффективного онлайн-продвижения
                                 компании.</p>
                         </div>
                     </li>
-                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center h-[200px]`}>
-                        <div className={`w-[100px] h-[100px]`}>
-                            <FaRegCalendarCheck size={100}/>
+                    <li className={`w-[45%] max-xl:w-full gap-[20px] flex items-center xl:h-[200px]`}>
+                        <div className={`w-[100px] h-[100px] max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px] max-md:flex max-md:items-center`}>
+                            <CiCalendar size={100} className={`max-md:w-[85px] max-md:h-[85px] max-sm:w-[50px] max-sm:h-[50px]`}/>
                         </div>
-                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px]`}>
-                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway`}>Дальнейшее
+                        <div className={`flex flex-col justify-start gap-[20px] h-full pt-[20px] max-xl:justify-around`}>
+                            <h3 className={`text-mainColor text-[28px] leading-[40px] website-creating-list-item-heading font-railway max-2xl:text-[26px] max-md:text-[24px] max-sm:text-[22px] max-md:leading-[32px]`}>Дальнейшее
                                 развитие после запуска</h3>
-                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr  max-md:mt-[10px]`}>Разработаем
+                            <p className={`text-[18px] leading-[28px] website-creating-list-item-descr max-sm:mt-0  max-md:mt-[10px] max-2xl:text-[16px] max-md:text-[15px] max-sm:text-[14px] max-sm:leading-[22px]`}>Разработаем
                                 дальнейшую стратегию продвижения, подберем самые эффективные инструменты, исходя из
                                 нашего опыта и анализа.</p>
                         </div>
