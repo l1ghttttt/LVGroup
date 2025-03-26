d3f43d2aa7c9fbe3776bf3c7fc983ad4a86158f1
nik0504030201 <nikita0504030201@gmail.com>
2025-03-27 00:28:22 +0500

    mobile adapting fix#2

--- src/app/brat-buryat/page.tsx
+++ src/app/brat-buryat/page.tsx
@@ -418,7 +418,7 @@ const BratBuryat = () => {
             </ul>
 
                 <section
-                    className={`overflow-hidden w-[100vw] relative left-0 top-0 comment-gradient max-sm:aspect-[16/14] max-md:aspect-[16/10] max-xl:aspect-[16/9] aspect-[16/8] flex justify-center items-center my-[120px] max-md:my-[40px] max-md:mt-[15px] max-md:flex-col max-md:py-[50px] overflow-x-hidden`}>
+                    className={`overflow-hidden w-[99vw] relative left-0 top-0 comment-gradient max-sm:aspect-[16/14] max-md:aspect-[16/10] max-xl:aspect-[16/9] aspect-[16/8] flex justify-center items-center my-[120px] max-md:my-[40px] max-md:mt-[15px] max-md:flex-col max-md:py-[50px] overflow-x-hidden`}>
                     <Comment image={`vlada.antonovaa.jpg`}
                              Xcoord={`22%`}
                              Ycoord={`10%`}
