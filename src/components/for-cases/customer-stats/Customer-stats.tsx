fd5b5a4948e83e7017930f61d396d03ba751f864
nik0504030201 <nikita0504030201@gmail.com>
2025-04-03 00:52:40 +0500

    VostokAvtoMash components fix

--- src/components/for-cases/customer-stats/Customer-stats.tsx
+++ src/components/for-cases/customer-stats/Customer-stats.tsx
@@ -12,7 +12,7 @@ interface CustomerStatsProps {
 
 const CustomerStats = ({statsList}: CustomerStatsProps) => {
     return (
-        <section>
+        <section className={`w-full`}>
             <ul className={`w-full relative flex items-start justify-start flex-wrap mx-[-30px] my-[45px] max-sm:mx-0 max-sm:block`}>
                 {statsList.map((stat, index) => (
                     <li className={`w-1/3 px-[30px] shrink-0 max-sm:w-full max-sm:p-0 max-sm:mb-[20px] max-lg:w-1/2 max-lg:mb-[30px]`} key={index}>
