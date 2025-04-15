c53fd5aaefb60d3ecb34566457a1ec1eaea28171
nik0504030201 <nikita0504030201@gmail.com>
2025-04-15 18:30:13 +0500

    Contacts

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+'use client'
+
+import React, {useEffect, useState} from 'react';
 import Link from "next/link";
 import { CiAirportSign1, CiBullhorn, CiUser } from "react-icons/ci";
+import {useTheme} from "next-themes";
+import Order from "@/components/order/Order";
 
 const Page = () => {
+    const { theme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) return null;
     return (
         <main className={`relative max-w-full w-full`}>
-            <section className="bg-cover bg-no-repeat bg-[url(https://static.nimax.ru/contacts/contacts1.png)]">
+            <section className="bg-cover bg-no-repeat">
+
+                <div className="video-background">
+                    <video autoPlay muted loop playsInline id="myVideo" className={theme == 'dark' ? '' : 'invert'}>
+                        <source src={`/ContactsBackground.mp4`} type="video/mp4"/>
+                    </video>
+                </div>
+
                 <div className={`bg-transparent relative p-ContactsHatPadding`}>
                     <div className={`sm:flex sm:items-start sm:justify-between max-lg:flex-wrap `}>
                         <div
@@ -18,10 +35,9 @@ const Page = () => {
                         </div>
                         <div
                             className={`shrink-0 flex flex-flow items-start justify-start leading-[1.2] w-1/3 max-xl:w-1/2 max-sm:w-full pl-ContactsLinkPaddingLeft mt-ContactsLinksMarginTop text-ContactsLinkTextSize`}>
-                            <Link href={""}> +7 (4212) 93-03-01 </Link>
-                            <Link href={""} className={"mt-[20px] mb-[30px] max-2xl:mb-[20px]"}> +7 (924)
-                                204-00-30 </Link>
-                            <Link href={"mt-[8px]"} className={``}>
+                            <Link href={"tel:+7 (4212) 93-03-01"}> +7 (4212) 93-03-01 </Link>
+                            <Link href={"tel:+7 (924) 204-00-30"} className={"mt-[20px] mb-[30px] max-2xl:mb-[20px]"}> +7 (924) 204-00-30 </Link>
+                            <div className={`mt-[8px]`}>
                                 <div
                                     className={`whitespace-pre-wrap w-ContactsUroInfWidth mb-ContactsUroInfMarginBottom mt-ContactsUroInfMarginTop`}>
                                     <div className={`mb-[10px] text-[calc(14px + var(--responsive)*10)]`}>
@@ -31,49 +47,51 @@ const Page = () => {
                                         ОГРНИП: 321272400043890
                                     </div>
                                 </div>
-                            </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div
                     className={`bg-transparent flex items-start justify-start flex-wrap relative gap-0 ml-[-1px] mb-[-1px]`}>
                     <div
-                        className={`shrink-0 bg-[rgba(31,31,31,.5)] ml-[1px] mb-[1px] relative overflow-hidden duration-500 transition-all w-ContactsCubeWidth h-ContactsCubeHeight hover:bg-[#1e1e1e]`}>
-                        <div className={`flex items-stretch justify-start flex-flow absolute z-[50] top-0 left-0 w-full h-full p-ContactsCubeInnerPadding`}>
-                            <div className={`leading-ContactsCubeTextLeading text-ContactsCubeTextSize h-1/4`}>
-                                Работаем с клиентами по всему миру
-                            </div>
-                            <div className={`w-full h-1/2 flex justify-center items-end`}>
+                        className={`shrink-0 ${theme == "dark" ? "bg-[rgba(31,31,31,.5)]" : "bg-[rgba(184,184,184,.5)]"} ml-[1px] mb-[1px] relative overflow-hidden duration-500 transition-all w-ContactsCubeWidth h-ContactsCubeHeight ${theme == "dark" ? "hover:bg-[#1e1e1e]" : "hover:bg-[#d1d1d1]"}`}>
+                        <div
+                            className={`flex items-stretch justify-start flex-flow absolute z-[50] top-0 left-0 w-full h-full p-ContactsCubeInnerPadding`}>
+                            <div className={`w-full h-4/5 flex justify-center items-center`}>
                                 <CiAirportSign1 size={100} className={`w-1/2 h-1/2`}/>
                             </div>
+                            <div className={`leading-ContactsCubeTextLeading text-ContactsCubeTextSize h-2/5`}>
+                                Работаем с клиентами по всему миру
+                            </div>
                         </div>
                     </div>
                     <div
-                        className={`shrink-0 bg-[rgba(31,31,31,.5)] ml-[1px] mb-[1px] relative overflow-hidden duration-500 transition-all w-ContactsCubeWidth h-ContactsCubeHeight hover:bg-[#1e1e1e]`}>
+                        className={`shrink-0 ${theme == "dark" ? "bg-[rgba(31,31,31,.5)]" : "bg-[rgba(184,184,184,.5)]"} ml-[1px] mb-[1px] relative overflow-hidden duration-500 transition-all w-ContactsCubeWidth h-ContactsCubeHeight  ${theme == "dark" ? "hover:bg-[#1e1e1e]" : "hover:bg-[#d1d1d1]"}`}>
                         <div
                             className={`flex items-stretch justify-start flex-flow absolute z-[50] top-0 left-0 w-full h-full p-ContactsCubeInnerPadding`}>
-                            <div className={`leading-ContactsCubeTextLeading text-ContactsCubeTextSize h-1/4`}>
-                                Всегда открыты к новым предложениям и сотрудничеству
-                            </div>
-                            <div className={`w-full h-1/2 flex justify-center items-end`}>
+                            <div className={`w-full h-4/5 flex justify-center items-center`}>
                                 <CiBullhorn size={100} className={`w-1/2 h-1/2`}/>
                             </div>
+                            <div className={`leading-ContactsCubeTextLeading text-ContactsCubeTextSize h-2/5`}>
+                                Всегда открыты к новым предложениям и сотрудничеству
+                            </div>
                         </div>
                     </div>
                     <div
-                        className={`shrink-0 bg-[rgba(31,31,31,.5)] ml-[1px] mb-[1px] relative overflow-hidden duration-500 transition-all w-ContactsCubeWidth h-ContactsCubeHeight hover:bg-[#1e1e1e]`}>
+                        className={`shrink-0 ${theme == "dark" ? "bg-[rgba(31,31,31,.5)]" : "bg-[rgba(184,184,184,.5)]"} ml-[1px] mb-[1px] relative overflow-hidden duration-500 transition-all w-ContactsCubeWidth h-ContactsCubeHeight  ${theme == "dark" ? "hover:bg-[#1e1e1e]" : "hover:bg-[#d1d1d1]"}`}>
                         <div
                             className={`flex items-stretch justify-start flex-flow absolute z-[50] top-0 left-0 w-full h-full p-ContactsCubeInnerPadding`}>
-                            <div className={`leading-ContactsCubeTextLeading text-ContactsCubeTextSize h-1/4`}>
-                                Рассматриваем как сотрудников в штат, так и работу на удалёнке
-                            </div>
-                            <div className={`w-full h-1/2 flex justify-center items-end`}>
+                            <div className={`w-full h-4/5 flex justify-center items-center`}>
                                 <CiUser size={100} className={`w-1/2 h-1/2`}/>
+                            </div>
+                            <div className={`leading-ContactsCubeTextLeading text-ContactsCubeTextSize h-2/5`}>
+                                Рассматриваем как сотрудников в штат, так и работу на удалёнке
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            <Order />
         </main>
     );
 };
