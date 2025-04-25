adc346765c3b39de5fb24e91d4f7a762f823e968
nik0504030201 <nikita0504030201@gmail.com>
2025-04-25 12:22:57 +0500

    cases list tags fix#2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Welcome from "@/widgets/welcome-page/Welcome";
 import ServicesListComponent from "@/widgets/services-list/Services-list";
 import Order from "@/components/order/Order";
+import CasesList from "@/components/cases-list/Cases-list";
 
 export default function Home() {
     return (
         <>
             <Welcome />
+            <CasesList onlyHomePage={true} />
             <ServicesListComponent />
             <Order />
         </>
